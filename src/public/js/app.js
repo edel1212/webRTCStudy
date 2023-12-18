47bb61b901a5640aa5868a3fed4d90eae90603d7
yoo <edel1212@naver.com>
2023-12-18 22:26:40 +0900

    웹에 캠연결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
 const socket = io();
+
+const myFace = document.querySelector("#myFace");
+
+let myStream;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    // 💬 접근 허용 창이 뜬다!
+    console.log(myStream);
+    myFace.srcObject = myStream;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMedia();
